db73015e859ab1bfbb814a44f80ff36b3c65ae36
Will Davis <wallace41290@gmail.com>
2024-03-01 19:52:05 -0600

    feat: general layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatToolbarModule, MatButtonModule, MatIconModule],
   selector: 'shipy-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-
+  constructor(private matIconReg: MatIconRegistry) {
+    this.matIconReg.setDefaultFontSetClass('material-symbols-outlined');
+  }
 }
