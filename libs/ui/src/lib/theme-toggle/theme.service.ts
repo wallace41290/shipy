8925df98dc6f9969626c0b5a8f60f27eaf9e24b8
Will Davis <wallace41290@gmail.com>
2024-09-22 09:32:03 -0400

    feat(theme): sync with local storage

--- libs/ui/src/lib/theme-toggle/theme.service.ts
+++ libs/ui/src/lib/theme-toggle/theme.service.ts
@@ -1,21 +1,41 @@
+import { coerceBooleanProperty } from '@angular/cdk/coercion';
 import { Injectable, signal } from '@angular/core';
 
-// TODO: save to local storage
-// TODO: initialize based on local storage value
 @Injectable({
   providedIn: 'root',
 })
 export class ThemeService {
+  static storageKey = 'shipy-theme-dark-mode';
+
   private _$darkMode = signal(false);
   $darkMode = this._$darkMode.asReadonly();
+
+  constructor() {
+    /** Read initial theme from local storage */
+    const storedTheme = this._getStoredThemeName();
+    this.setDarkMode(coerceBooleanProperty(storedTheme));
+  }
 
   setDarkMode(isDarkMode: boolean) {
     this._$darkMode.set(isDarkMode);
     this._applyCssThemeClass();
+    try {
+      window.localStorage[ThemeService.storageKey] = isDarkMode;
+    } catch {
+      console.error('[ThemeService] Failed store theme in local storage');
+    }
   }
 
   toggleTheme() {
     this.setDarkMode(!this._$darkMode());
+  }
+
+  private _getStoredThemeName(): string | null {
+    try {
+      return window.localStorage[ThemeService.storageKey] || null;
+    } catch {
+      return null;
+    }
   }
 
   private _applyCssThemeClass() {
