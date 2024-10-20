57720da05843c42936980400f17f9c0de17ffc50
Will Davis <wallace41290@gmail.com>
2024-10-20 08:04:54 -0400

    fix(filter-reset): only reset filter query params

--- libs/search/src/lib/filters-reset/filters-reset.component.ts
+++ libs/search/src/lib/filters-reset/filters-reset.component.ts
@@ -8,11 +8,12 @@ import {
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'shipy-filters-reset',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './filters-reset.component.html',
   styleUrl: './filters-reset.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
