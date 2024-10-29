c119d8f042762fd71fa2c55faf1ca7bcd6598423
Will Davis <wallace41290@gmail.com>
2024-10-29 14:25:29 -0400

    fix(search): set default search to a month out from current date

--- libs/utils/src/lib/get-next-month.ts
+++ libs/utils/src/lib/get-next-month.ts
@@ -1,8 +1,9 @@
 export function getNextMonth(): Date {
   const now = new Date();
+  const day = now.getDate();
   if (now.getMonth() === 11) {
-    return new Date(now.getFullYear() + 1, 0, 1);
+    return new Date(now.getFullYear() + 1, 0, day);
   } else {
-    return new Date(now.getFullYear(), now.getMonth() + 1, 1);
+    return new Date(now.getFullYear(), now.getMonth() + 1, day);
   }
 }
